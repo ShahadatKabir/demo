b95aa424c5b90ddb5d94881f7833b741571ee082
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2022-04-05 11:43:50 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = require("express")();
+const PORT = process.env.PORT||3000;
+app.get("", (req, res) => {
+res.send("Hello world");
+});
+app.listen(PORT, () => {
+    console.log('App up at port 3000');
+    });
